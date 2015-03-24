7c6f98624a93bdc99011171d9c09a75825db8486
Patrick Stoica <hipatark@gmail.com>
2015-03-23 23:07:53 -0400

    checkpoint. use Tone.js for timing.

--- entry.js
+++ entry.js
@@ -1,89 +1,81 @@
 var Rx = require('rx');
 var Tone = require('tone');
-var WAAClock = require('waaclock');
 
 var tone = new Tone();
-
-var audioContext = new AudioContext();
-var clock = new WAAClock(audioContext);
-clock.start();
+Tone.Transport.bpm.value = 80;
+Tone.Transport.start();
 
 var metronome = new Rx.Subject();
+metronome.subscribe(() => {
+  console.log('tick');
+});
 
-clock.callbackAtTime(function() {
-  metronome.onNext({
-    playbackTime: audioContext.currentTime
-  });
-}, 1).repeat(1);
+Tone.Transport.setInterval(function(time){
+  metronome.onNext(time);
+}, "4n");
 
-
-function createNode(source, options) {
+function createSynth() {
   var synth = new Tone.MonoSynth({
-    filterEnvelope: {
-      min: 2000,
-      max: 2000
+    filter: {
+      type: 'allpass'
     },
     oscillator: {
-      type: "square"
+      type: 'triangle'
     }
   });
 
   synth.toMaster();
+  return synth;
+}
 
-  function play(e, options, onEnd) {
-    var note = options.note;
-    var dur = options.dur;
-    var at = options.at;
+let _nodes = 0;
 
-    clock.callbackAtTime(function () {
-      onEnd({
-        playbackTime: audioContext.currentTime
-      });
-      synth.triggerAttackRelease(tone.midiToNote(note), dur);
-    }, e.playbackTime + at);
-  }
+function createNode(source, options) {
+  const id = _nodes++;
+  const synth = createSynth();
+  const subject = new Rx.Subject();
+  const { note, dur, when } = options;
 
-  var subject = new Rx.Subject();
+  source.subscribe(function (time) {
+    Tone.Transport.setTimeout(time => {
+      console.log(id);
+      subject.onNext(time);
 
-  source.subscribe(function (e) {
-    console.log('observer');
-    play(e, options, function (e) {
-      subject.onNext(e);
-    });
+      synth.triggerAttackRelease(tone.midiToNote(note + 12), dur, time);
+    }, when);
   });
 
   return subject;
 }
 
-// Create observable to handle the messages
 
 var node1 = createNode(metronome, {
   note: 60,
-  at: 0.5,
-  dur: 0.1
+  when: '8n',
+  dur: '8n'
 });
 
 var node2 = createNode(node1, {
   note: 62,
-  at: 0.5,
-  dur: 0.1
+  when: '16n',
+  dur: '8n'
 });
 
 var node3 = createNode(node2, {
   note: 64,
-  at: 0.75,
-  dur: 0.1
+  when: '16n',
+  dur: '8n'
 });
 
 var node4 = createNode(node3, {
   note: 72,
-  at: 0.25,
-  dur: 0.1
+  when: '8n',
+  dur: '8n'
 });
 
 var node5 = createNode(node4, {
   note: 67,
-  at: 0.125,
-  dur: 0.1
+  when: '4n',
+  dur: '8n'
 });
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   devtool: 'eval',
-  entry: './entry',
+  entry: './entry.js',
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader?experimental&optional=runtime',
+        exclude: /node_modules/
+      }
+    ]
+  },
   output: {
     path: __dirname + '/dist',
     filename: 'bundle.js'
